4ea2d6fad883afbd32777a705b460f491a8123e3
tnebes <47086978+tnebes@users.noreply.github.com>
2020-10-20 13:45:03 +0200

    used modulo, lists, and flow control to solve puzzle

---
+++ chapter_7_arrays/exercise_7_23/LockerPuzzle.java
@@ -0,0 +1,37 @@
+/* School has 100 lockers and students
+ * Lockers are closed on the first day.
+ * The first student opens every locker
+ * The second student closes every second locker
+ * The third student inverts every third locker
+ * The fourth student inverts every fourth locker
+ * continues until the 100th student changes the 100th lock
+ * Which lockers are open at the end?
+ */
+
+public class LockerPuzzle {
+	public static void main(String[] args) {
+		
+		boolean[] lockerStatus = new boolean[100];
+		
+		// first student opens every locker
+		for (int i = 0; i < lockerStatus.length; i++)
+			lockerStatus[i] = true;
+
+		//all the remaining students invert all the other lockers.
+		for (int i = 0; i < lockerStatus.length; i++) {
+			for (int j = 2; j < lockerStatus.length; j++) {
+				if (i % j == 0) {
+					if (lockerStatus[i])
+						lockerStatus[i] = false;
+					else
+						lockerStatus[i] = true;
+				}
+			}
+		}
+		//which lockers are open?
+		for (int i = 0; i < lockerStatus.length; i++) {
+			if (lockerStatus[i])
+				System.out.printf("Locker %d is open.\n", i);
+		}
+	}
+}
