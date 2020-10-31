a358254f7cd756ca9e5d12f6112deed85c6c07bf
tnebes <47086978+tnebes@users.noreply.github.com>
2020-10-31 19:06:00 +0100

    Produced a 3d array that contains all permutations of coin tosses in a 3x3 2d array. Simulates decimal to binary converter using coin tosses.

--- chapter_8_multiDimensionalArrays/exercise_8_11/NineHeadsTails.java
+++ chapter_8_multiDimensionalArrays/exercise_8_11/NineHeadsTails.java
@@ -11,28 +11,48 @@
 
 public class NineHeadsTails {
 	public static void main(String[] args) {
+		java.util.Scanner input = new java.util.Scanner(System.in);
 		final int MATRIX_ROWS = 3, MATRIX_COLUMNS = 3;
+		int choice;
+
 		int[][][] matrixCollection = new int[
-			Math.pow(MATRIX_ROWS * MATRIX_COLUMNS, 2)]
+			(int) Math.pow(2, MATRIX_ROWS * MATRIX_COLUMNS)]
 			[MATRIX_ROWS][MATRIX_COLUMNS];
 		populateMatrices(matrixCollection);
+
+		do {
+			System.out.printf("Enter a number between 0 and %d: ",
+				matrixCollection.length - 1);
+			choice = input.nextInt();
+		} while (choice < 0 || choice >= matrixCollection.length);
+		printMatrix(choice, matrixCollection);
 	}
 
 	/** populates the 3d array with permutations */
 	public static void populateMatrices(int[][][] matrixCollection) {
-		int divider = 0;
+		int number;
 		for (int i = 0; i < matrixCollection.length; i++) {
-			for (int j = 0; j < matrixCollection[i].length; j++) {
-				divider = i;
-				for (int k = 0; k < matrixCollection[k].length; k++) {
-					if (divider != 0) {
-											
-					} else {
-						matrixCollection[i][j][k] = 0;
-						break;
-					}
+			number = i;
+			for (int j = matrixCollection[i].length - 1; j >= 0; j--) {
+				for (int k = matrixCollection[i][j].length - 1; k >= 0; k--) {
+					matrixCollection[i][j][k] = number % 2;
+					number /= 2;
 				}
+				if (number == 0)
+					break;
 			}
 		}
 	}
+
+	/** prints the contents of one matrix from a collection of matrices.*/
+	public static void printMatrix(int choice, int[][][] matrixCollection) {
+		for (int i = 0; i < matrixCollection[choice].length; i++) {
+			for (int j = 0; j < matrixCollection[choice][i].length; j++) {
+				System.out.printf("%c ",
+					matrixCollection[choice][i][j] == 1 ? 'T' : 'H');
+			}
+			System.out.printf("\n");
+		}
+	}
+
 }
