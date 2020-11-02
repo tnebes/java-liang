2b7b8409736562209b1e9a5c31d8a632c1965033
tnebes <47086978+tnebes@users.noreply.github.com>
2020-11-02 09:52:54 +0100

    Started working on producing a 2d matrix and finding identical numbers in row/column/diagonals.

---
+++ chapter_8_multiDimensionalArrays/exercise_8_14/ExploreMatrix.java
@@ -0,0 +1,126 @@
+/* write a program that prompts the user to enter the length
+ * of a square matrix, randomly fills in 0s and 1s into the matrix,
+ * prints the matrix
+ * finds the rows
+ * columns
+ * and diagonals with all 0s and 1s
+ */
+
+public class ExploreMatrix {
+	public static void main(String[] args) {
+
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		java.util.Random rng = new java.util.Random();
+		System.out.print("Enter the size of the matrix: ");
+		int arraySize = input.nextInt();
+		int[][] array = new int[arraySize][arraySize];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = rng.nextInt(2);
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				System.out.printf("%d ", array[i][j]);
+			}
+			System.out.printf("\n");
+		}
+		findIdentical(array);
+	}
+
+	public static void findIdentical(int[][] array) {
+		findHorizontal(array);
+		findVertical(array);
+		findDiagonal(array);
+	}
+
+	public static void findHorizontal(int[][] array) {
+		int counter = 0, identical = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 1; j < array[i].length; j++) {
+				if (array[i][0] == array[i][j]) {
+					counter++;
+				} else {
+					counter = 0;
+					break;
+				}
+				if (counter == 2) {
+					System.out.printf("All %d on row %d.\n",
+						array[i][0], i);
+					identical++;
+				}
+			}
+			counter = 0;
+		}
+		if (identical == 0) {
+			System.out.printf("No same numbers on a row.\n");
+		}
+	}
+
+	public static void findVertical(int[][] array) {
+		int counter = 0, identical = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (array[0][i] == array[j][i]) {
+					counter++;
+				} else {
+					counter = 0;
+					break;
+				}
+				if (counter == 2) {
+					System.out.printf("All %d on column %d\n",
+						array[0][i], j);
+					identical++;
+				}
+			}
+			counter = 0;
+		}
+		if (identical == 0) {
+			System.out.printf("No same number on a columns.\n");
+		}
+	}
+
+	public static void findDiagonal(int[][] array) {
+		int counter = 0, identical = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[0][0] == array[i][i])
+				counter++;
+			else {
+				counter = 0;
+				break;
+			}
+			if (counter == 2) {
+				System.out.printf("All %ds on the major diagonal\n",
+					array[0][0]);
+				identical++;
+				counter = 0;
+			}
+			counter = 0;
+		}
+		if (identical == 0)
+			System.out.printf("No same numbers on the major diagonal.\n");
+
+		counter = 0;
+	   	identical = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[0][array.length - 1]
+				== array[i][array.length - 1 - i]) {
+					counter++;
+				} else {
+					counter = 0;
+					break;
+				}
+			if (counter == 2) {
+				System.out.printf("All %ds on sub-diagonal\n",
+					array[0][array.length - 1]);
+				identical++;
+			}
+		}
+		counter = 0;
+		if (identical == 0)
+			System.out.printf("No same numbers on the sub-diagonal\n");
+	}
+
+}
