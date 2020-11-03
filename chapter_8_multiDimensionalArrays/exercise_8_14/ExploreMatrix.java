83de7424a1bd3c7a16a390cb3725bfc84f40eb5e
tnebes <47086978+tnebes@users.noreply.github.com>
2020-11-03 10:40:47 +0100

    Generation of a 2d matrix containing random values. Checking each row, column and diagonal for identical numbers.

--- chapter_8_multiDimensionalArrays/exercise_8_14/ExploreMatrix.java
+++ chapter_8_multiDimensionalArrays/exercise_8_14/ExploreMatrix.java
@@ -11,29 +11,35 @@ public class ExploreMatrix {
 
 		java.util.Scanner input = new java.util.Scanner(System.in);
 		java.util.Random rng = new java.util.Random();
-		System.out.print("Enter the size of the matrix: ");
-		int arraySize = input.nextInt();
-		int[][] array = new int[arraySize][arraySize];
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				array[i][j] = rng.nextInt(2);
+		do {
+			System.out.print("Enter the size of the matrix: ");
+			int arraySize = input.nextInt();
+			int[][] array = new int[arraySize][arraySize];
+			for (int i = 0; i < array.length; i++) {
+				for (int j = 0; j < array[i].length; j++) {
+					array[i][j] = rng.nextInt(2);
+				}
 			}
-		}
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				System.out.printf("%d ", array[i][j]);
+			for (int i = 0; i < array.length; i++) {
+				for (int j = 0; j < array[i].length; j++) {
+					System.out.printf("%d ", array[i][j]);
+				}
+				System.out.printf("\n");
 			}
-			System.out.printf("\n");
-		}
-		findIdentical(array);
+			findIdentical(array);
+			System.out.print("Another exploration? y/n ");
+		} while (input.next().equals("y"));
 	}
 
+	/** calls various methods to check whether there exist
+	 * same numbers in any row, column or diagonal.*/
 	public static void findIdentical(int[][] array) {
 		findHorizontal(array);
 		findVertical(array);
 		findDiagonal(array);
 	}
 
+	/** checks whether there are same numbers on any row.*/
 	public static void findHorizontal(int[][] array) {
 		int counter = 0, identical = 0;
 
@@ -45,8 +51,8 @@ public class ExploreMatrix {
 					counter = 0;
 					break;
 				}
-				if (counter == 2) {
-					System.out.printf("All %d on row %d.\n",
+				if (counter == array.length - 1) {
+					System.out.printf("All %ds on row %d.\n",
 						array[i][0], i);
 					identical++;
 				}
@@ -58,46 +64,48 @@ public class ExploreMatrix {
 		}
 	}
 
+	/** checks whether there are same numbers on any column.*/
 	public static void findVertical(int[][] array) {
 		int counter = 0, identical = 0;
 
 		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
+			for (int j = 1; j < array[i].length; j++) {
 				if (array[0][i] == array[j][i]) {
 					counter++;
 				} else {
 					counter = 0;
 					break;
 				}
-				if (counter == 2) {
-					System.out.printf("All %d on column %d\n",
+				if (counter == array.length - 1) {
+					System.out.printf("All %ds on column %d\n",
 						array[0][i], j);
 					identical++;
+					counter = 0;
 				}
 			}
 			counter = 0;
 		}
 		if (identical == 0) {
-			System.out.printf("No same number on a columns.\n");
+			System.out.printf("No same number on a column.\n");
 		}
 	}
 
+	/** checks for identical numbers on major and sub-diagonal.*/
 	public static void findDiagonal(int[][] array) {
 		int counter = 0, identical = 0;
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 1; i < array.length; i++) {
 			if (array[0][0] == array[i][i])
 				counter++;
 			else {
 				counter = 0;
 				break;
 			}
-			if (counter == 2) {
+			if (counter == array.length - 1) {
 				System.out.printf("All %ds on the major diagonal\n",
 					array[0][0]);
 				identical++;
 				counter = 0;
 			}
-			counter = 0;
 		}
 		if (identical == 0)
 			System.out.printf("No same numbers on the major diagonal.\n");
@@ -112,13 +120,13 @@ public class ExploreMatrix {
 					counter = 0;
 					break;
 				}
-			if (counter == 2) {
+			if (counter == array.length - 1) {
 				System.out.printf("All %ds on sub-diagonal\n",
 					array[0][array.length - 1]);
 				identical++;
+				counter = 0;
 			}
 		}
-		counter = 0;
 		if (identical == 0)
 			System.out.printf("No same numbers on the sub-diagonal\n");
 	}
