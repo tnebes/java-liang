353c22d01ce47e62a909a49d84acf01213ced5ac
tnebes <47086978+tnebes@users.noreply.github.com>
2020-11-06 19:06:23 +0100

    Started working on pattern recognition

---
+++ chapter_8_multiDimensionalArrays/exercise_8_19/PatternRecognition.java
@@ -0,0 +1,17 @@
+/* Write a method that tests whether a 2d array has four consecutive
+ * nmbers of the same value, either horizontally, vertically or diagonally.
+ * Write a test program that prompts the user to enter the number of rows
+ * and columns of a 2d array and then the values in the array
+ * and displays true if the array contains four consecutive numbers
+ * with the same value. Otherwise, the program displays false.
+ */
+
+public class PatternRecognition {
+	public static void main(String[] args) {
+	
+	}
+
+	public static boolean isConsecutiveFour(int[][] values) {
+	
+	}
+}
