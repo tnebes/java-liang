92e94572b2a06e6e6db71d9e80ff18f0ce67fe6c
tnebes <47086978+tnebes@users.noreply.github.com>
2021-01-10 20:13:18 +0100

    Sandbox

--- chapter_10_oo_thinking/sandbox/JavaString.java
+++ chapter_10_oo_thinking/sandbox/JavaString.java
@@ -6,6 +6,7 @@ public class JavaString {
 
 	public static void main(String[] args) {
 		
+		// testing cursed stuff
 		String testString = "123456789";
 		int sum = 0;
 		for (int i = 0; i < testString.length(); i++) {
@@ -13,8 +14,25 @@ public class JavaString {
 		}
 		System.out.printf("%d\n", sum);
 		
+		// testing replace
+		testString.replace('1', '9');
+		System.out.printf("%s\n", testString);
+		System.out.printf("%s\n", testString.replace('9', '1'));
+		testString = testString.replace('5', '4');
+		System.out.printf("%s\n", testString);
 		
+		testString = "hello#my#dear#friends";
+		System.out.print(testString.split("#") + "\n");
+		String[] tokens = testString.split("#");
+		for (int i = 0; i < tokens.length; i++) {
+			System.out.printf("%s\t", tokens[i]);
+		}
 		
+		// are both equal?
+		String str = String.valueOf(new char[]{'J', 'a', 'v', 'a'});
+		str = new String(new char[]{'J', 'a', 'v', 'a'});
+		//
+		System.out.printf("%s\n", str);
 	}
 	
 }
