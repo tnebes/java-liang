91af7ecb0db0844fe920d1b489552b2b27432cb5
tnebes <47086978+tnebes@users.noreply.github.com>
2021-01-16 14:35:38 +0100

    Program checks whether a point is contained within a triangle and whether a triangle is contained within a triangle

--- chapter_10_oo_thinking/exercise_10_12/Triangle2D.java
+++ chapter_10_oo_thinking/exercise_10_12/Triangle2D.java
@@ -2,19 +2,29 @@
  * Define a Triangle2D class that contains
  * p1, p2, and p3 of MyPoint type
  * Constructor with points and no-arg constructor
- * getArea() method
- * getPerimeter() method
+ * getArea() method : double
+ * getPerimeter() method : double
  * contains(myPoint p) : boolean
  * contains(Triangle2D t) : boolean
  * overlaps(Triangle2D t) : boolean
  * 
- * 
+ * Write a test program that creates a Triangle2D object t1
+ * using the constructor
+ * 	new Triangle2D(new MyPoint(2.5, 2),
+ * 	new MyPoint(4.2, 3),
+ * 	new MyPoint(5, 3.5)),
+ * displays its area and perimeter,
+ * and displays the result of
+ * 	t1.contains(3, 3),
+ * 	r1.contains(new Triangle2D(new MyPoint(2.9, 2), new MyPoint(4, 1), MyPoint(1, 3.4))),
+ * 	and t1 .overlaps(new Triangle2D(new MyPoint(2, 5.5), new MyPoint (4, –3), MyPoint(2, 6.5)))
  */
 public class Triangle2D {
 
 	private MyPoint p1, p2, p3;
 	private double a, b, c; // sides of the triangle
 	private double area, perimeter;
+	private final double EPSILON = 1e-14;
 	
 	/**
 	 * no-arg constructor
@@ -33,43 +43,97 @@ public class Triangle2D {
 		this.p1 = p1;
 		this.p2 = p2;
 		this.p3 = p3;
-		this.a = distance(p1, p2);
-		this.b = distance(p2, p3);
-		this.c = distance(p3, p1);
-		this.area = calculateArea();
+		this.a = p1.distance(p2);
+		this.b = p2.distance(p3);
+		this.c = p3.distance(p1);
 		this.perimeter = calculatePerimeter();
+		this.area = calculateArea();		
 	}
 	
 	/**
-	 * Calculates the distance between 2 MyPoint objects
-	 * @param point1
-	 * @param point2
+	 * Calculates the perimeter when the object is instantiated
 	 * @return double
 	 */
-	private double distance(MyPoint point1, MyPoint point2) {
-		return Math.sqrt(Math.pow((point1.getX() - point2.getX()), 2) + Math.pow((point1.getY() - point2.getY()), 2));
-	}
-	
-	private double calculateArea() {
-		// uses Heron's formula
-		double halfPerimeter = this.getPerimeter() / 2;
-		return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
-	}
-	
 	private double calculatePerimeter() {
 		return a + b + c;
 	}
 	
+	/**
+	 * Calculates the area when the object is instantiated
+	 * @return double
+	 */
+	private double calculateArea() {
+		// uses Heron's formula
+		double halfPerimeter = perimeter / 2d;
+		return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+	}
+	
+	/**
+	 * Returns the area of the triangle
+	 * @return double
+	 */
 	public double getArea() {
 		return area;
 	}
 	
+	/**
+	 * Returns the perimeter of the triangle
+	 * @return double
+	 */
 	public double getPerimeter() {
 		return perimeter;
 	}
 	
+	/**
+	 * Returns the MyPoint object
+	 * @return MyPoint
+	 */
+	public MyPoint getP1() {
+		return p1;
+	}
+	
+	/**
+	 * Returns the MyPoint object
+	 * @return
+	 */
+	public MyPoint getP2() {
+		return p2;
+	}
+	
+	/**
+	 * Returns the MyPoint object
+	 * @return
+	 */
+	public MyPoint getP3() {
+		return p3;
+	}
+
+	/**
+	 * Checks whether a point is contained within the triangle.
+	 * This is done by instantiating 3 triangles whose 1 point is the given point.
+	 * If the area of the three triangles is similar to the area of the original triangle
+	 * the point is in the triangle.
+	 * @param point
+	 * @return boolean
+	 */
 	public boolean contains(MyPoint point) {
-		return false;
+		double area1 = new Triangle2D(point, p1, p2).getArea();
+		double area2 = new Triangle2D(point, p2, p3).getArea();
+		double area3 = new Triangle2D(point, p3, p1).getArea();
+		return Math.abs((area1 + area2 + area3) - this.getArea()) < EPSILON;
+	}
+	
+	/**
+	 * Checks whether the triangle contains another triangle.
+	 * @param triangle
+	 * @return boolean
+	 */
+	public boolean contains(Triangle2D triangle) {
+		return contains(triangle.getP1()) && contains(triangle.getP2()) && contains(triangle.getP3());
+	}
+	
+	public boolean overlaps(Triangle2D triangle) {
+		return contains(triangle.getP1()) && contains(triangle.getP2()) && contains(triangle.getP3());
 	}
 	
 	
