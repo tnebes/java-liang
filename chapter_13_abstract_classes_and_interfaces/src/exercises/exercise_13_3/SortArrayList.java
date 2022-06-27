fa27626237c8462951ec2ff04b37357602b26e93
tnebes <tomislav.nebes@gmail.com>
2022-06-27 10:14:32 +0200

    Exercise 13 3

---
+++ chapter_13_abstract_classes_and_interfaces/src/exercises/exercise_13_3/SortArrayList.java
@@ -0,0 +1,28 @@
+package exercises.exercise_13_3;
+
+import exercises.exercise_13_2.ShuffleArrayList;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class SortArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<Number> list = new ArrayList<>();
+        IntStream.range(0, 100).forEach(list::add);
+        System.out.println("Original: " + list);
+        ShuffleArrayList.MyShuffler.shuffle(list);
+        System.out.println("Shuffled: " + list);
+        MyArrayListSorter.sort(list);
+        System.out.println("Sorted: " + list);
+    }
+
+    public static class MyArrayListSorter {
+
+        public static void sort(ArrayList<Number> list) {
+            list.sort(Comparator.comparingDouble(Number::doubleValue));
+        }
+    }
+
+}
